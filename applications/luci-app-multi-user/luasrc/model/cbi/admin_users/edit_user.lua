81c8cc220c6dc206e56afac1bd84a904305aed57
Fire-WRT <scottmercer19@gmail.com>
2017-03-31 10:10:16 -0400

    Update edit_user.lua
    Added ability to scan for all menu entries, this should allow menu control of any packages installed

--- applications/luci-app-multi-user/luasrc/model/cbi/admin_users/edit_user.lua
+++ applications/luci-app-multi-user/luasrc/model/cbi/admin_users/edit_user.lua
@@ -3,10 +3,12 @@
 -- Licensed to the public under the Apache License 2.0.
 
 local utl = require "luci.util"
+local dsp = require "luci.dispatcher"
 
 m = Map("users", arg[1]:upper(), translate("User Configuration Options"))
 
 local fs = require "nixio.fs"
+local menu = dsp.load_menus()
 local groups = {"users", "admin", "other"}
 local usw = require "luci.users"
 require "uci"
@@ -19,6 +21,11 @@ end
 
 s = m:section(NamedSection, arg[1], "user")
 s.addremove = false
+
+
+function firstToUpper(str)
+    return (str:gsub("^%l", string.upper))
+end
 
 function s.parse(self, ...)
   NamedSection.parse(self, ...)
@@ -35,53 +42,33 @@ o:value("Enabled", "Enabled")
 o:value("Disabled", "Disabled")
 o.default = "Enabled"
 
-o = s:option(Flag, "Status_menus", translate("Enable Status Menu"))
-o.rmempty = true
-o.disabled = "disabled" 
-o.enabled = "Status_menus"
+network_menu = s:option(Flag, "network_menus", translate("Enable Network Menus"))
+network_menu.rmempty = true 
+network_menu.disabled = "disabled" 
+network_menu.enabled = "Network_menus"
 
-status_subs = s:option(MultiValue, "Status_subs")
-status_subs.rmempty = true
-status_subs:depends("Status_menus", "Status_menus")
---status_subs:value("Overview", "Overview")
-status_subs:value("Firewall", "Firewall")
-status_subs:value("Routes", "Routes")
-status_subs:value("System_log", "System Log") 
-status_subs:value("Kernel_log", "Kernel Log")
-status_subs:value("Processes", "Processes")
-status_subs:value("Realtime_graphs", "Realtime Graphs")
-
-o = s:option(Flag, "System_menus", translate("Enable System Menu"))
-o.rmempty = true 
-o.disabled = "disabled" 
-o.enabled = "System_menus"
-
-system_subs = s:option(MultiValue, "System_subs")
-system_subs.rmempty = true
-system_subs:depends("System_menus", "System_menus")
-system_subs:value("System", "System")
---system_subs:value("Administration", "Administration")
-system_subs:value("Software", "Software")
-system_subs:value("Startup", "Startup")
-system_subs:value("Scheduled_tasks", "Scheduled Tasks")
-system_subs:value("Leds", "LED Configuration")
-system_subs:value("Firmware", "Backup / Flash Firmware")
-system_subs:value("Reboot", "Reboot")
-
-o = s:option(Flag, "Network_menus", translate("Enable Network Menu"))
-o.rmempty = true 
-o.disabled = "disabled" 
-o.enabled = "Network_menus"
-
-network_subs = s:option(MultiValue, "Network_subs")
+network_subs = s:option(MultiValue, "network_subs")
 network_subs.rmempty = true
-network_subs:depends("Network_menus", "Network_menus")
+network_subs:depends("network_menus", "Network_menus")
 network_subs:value("Interfaces", "Interfaces")
 network_subs:value("Wifi", "Wifi")
 network_subs:value("Switch", "Switch")
 network_subs:value("Dhcp", "DHCP and DNS")
 network_subs:value("Firewall", "Firewall")
 network_subs:value("Diagnostics", "Diagnostics")
+
+for i,v in pairs(menu) do
+  o = s:option(Flag, i.."_menus", translate("Enable ".. firstToUpper(i).." Menus"))
+  o.disabled = "disabled" 
+  o.enabled = i:gsub("^%l", string.upper).."_menus"
+  new = s:option(MultiValue, i.."_subs")
+  for j,k in ipairs(v) do
+    new:depends(i.."_menus", i:gsub("^%l", string.upper).."_menus")
+   if k ~= "Status" and k ~= "Overview" and k ~= "Services" then
+     new:value(k)
+   end
+  end
+end
 
 m.redirect = luci.dispatcher.build_url("admin/users/users")
 
